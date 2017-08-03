ce9ab228b193c516551af02a6e9e1bf781e46512
Luka Rahne <luka.rahne@gmail.com>
2017-08-03 22:06:35 +0200

    extra tests for intrinsics

--- t/t_vec_intrinsics.cpp
+++ t/t_vec_intrinsics.cpp
@@ -10,6 +10,17 @@
 using namespace rts;
 namespace mm = rts::vec_intrinsics;
 
+template<typename T,typename A>
+static vec<T,A> range(T i0,T step){
+    vec<T,A> r;
+    T t(i0);
+    for (int i=0;i<A::width;++i) {
+       r[i] = t;
+       t += step;
+    }
+    return r;
+}
+
 template <class T, class A, class A1, class VAL> void require_eq (const vec<T,A> & u, const vec<T,A1> & v, VAL val) {
   SECTION(val) {
     for (int i=0;i<A::width;++i) {
@@ -41,6 +52,13 @@ template <class T, class A, class A1, class VAL> void require_eq (const vec<T,A>
                 #val0 "," #val1 ); \
   }
 
+#define RTS_TEST2R(fun,T,r0,r1) \
+  SECTION(#fun " " #T) { \
+    require_eq( mm::fun(range<T,Arch0>r0, range<T,Arch0>r1 ), \
+                mm::fun(range<T,Arch1>r0, range<T,Arch1>r1 ), \
+               #r0 "," #r1); \
+}
+
 #define RTS_TEST2T(fun,T,T1,val0,val1) \
   SECTION(#fun " " #T "," #T1) { \
     require_eq( mm::fun(vec<T,Arch0>(val0), vec<T1,Arch0>(val1)), \
@@ -53,20 +71,33 @@ template <class Arch0,class Arch1>
 void compare_arch () {
   using std::int32_t;
 
+  SECTION("range"){
+     using tg8 = target::generic<8>;
+     require_eq(vec<int32_t,tg8>({1,2,3,4,5,6,7,8}),range<int32_t,tg8>(1,1),"range-int");
+     require_eq(vec<float,tg8>({1,3,5,7,9,11,13,15}),range<float,tg8>(1,2),"range-float");
+  }
+
   SECTION("add"){
     RTS_TEST2(add, float, 1.23, 4.56)
+    RTS_TEST2R(add, float, (1.23,1), (4.56,-1.0))
     RTS_TEST2(add, int32_t, 23, 45)
+    RTS_TEST2R(add, int32_t, (23,5), (45,10))
   }
   SECTION("sub"){
     RTS_TEST2(sub, float, 1.23, 4.56)
+    RTS_TEST2R(sub, float, (1.23,1), (4.56,-1.0))
     RTS_TEST2(sub, int32_t, 23, 45)
+    RTS_TEST2R(sub, int32_t, (23,5), (45,10))
   }
   SECTION("mul"){
     RTS_TEST2(mul, float, 1.23, 4.56)
+    RTS_TEST2R(mul, float, (1.23,1), (4.56,-1.0))
     RTS_TEST2(mul, int32_t, 23, 45)
+    RTS_TEST2R(mul, int32_t, (23,5), (45,10))
   }
   SECTION("div"){
     RTS_TEST2(div, float, 1.23, 4.56)
+    RTS_TEST2R(div, float, (1.23,1), (4.56,-1.0))
   }
 
   std::int32_t s1 = 0b0101010101010101010101010101010;
